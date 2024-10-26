6e46a25af158fb5c2180d242e6964b7286eacdcf
Gian <gianfelipe93@gmail.com>
2024-10-26 20:13:51 +1100

    removing empty lines to organise the code

--- react_ws_src/src/helpers/serialize_params.js
+++ react_ws_src/src/helpers/serialize_params.js
@@ -1,7 +1,5 @@
 const serialize_params = function (obj) {
-
-	return Object.keys(obj).map( (k) => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]) ).join('&')
-	// return Object.keys(obj).reduce(function(a,k){a.push(encodeURIComponent(k)+'='+encodeURIComponent(obj[k]));return a},[]).join('&')
+	return Object.keys(obj).map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&')
 }
 
 export default serialize_params
