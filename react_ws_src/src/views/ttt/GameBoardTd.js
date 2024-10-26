f6b822d4629bc9adc645f01d4b791eb8f20f70f7
Gian <gianfelipe93@gmail.com>
2024-10-26 20:36:50 +1100

    Created GameBoardTdConstructor so I pass the onClick function just once instead of passing it multiple times to each GameBoardTd

--- react_ws_src/src/views/ttt/GameBoardTd.js
+++ react_ws_src/src/views/ttt/GameBoardTd.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const GameBoardTd = (props) => {
-  const { index, onClick, className = '', typeOfCell } = props
+const GameBoardTdConstructor = (onClick) => {
+  const GameBoardTd = (props) => {
+    const { index, className = '', typeOfCell } = props
 
-  const cIndex = `c${index}`
-  const cellIcon = typeOfCell === 'x' ? <i className="fa fa-times fa-5x"></i> : <i className="fa fa-circle-o fa-5x"></i>
+    const cIndex = `c${index}`
+    const cellIcon = typeOfCell === 'x' ? <i className="fa fa-times fa-5x"></i> : <i className="fa fa-circle-o fa-5x"></i>
 
-  return <td id={`game_board-${cIndex}`} ref={cIndex} onClick={onClick} className={className}>
-    {cellIcon}
-  </td>
+    return <td id={`game_board-${cIndex}`} ref={cIndex} onClick={onClick} className={className}>
+      {cellIcon}
+    </td>
+  }
+
+  return GameBoardTd
 }
 
-export default GameBoardTd;
+export default GameBoardTdConstructor;
